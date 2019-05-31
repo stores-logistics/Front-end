bdf2e17d822fc776233070833250d498bd502951
Iván Herrera <biherrerap@unal.edu.co>
2019-05-31 00:10:48 -0500

    Personalized Home

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -14,9 +14,9 @@ class Home extends React.Component{
                         <div class="frontside"> 
                             <div class="card">
                                 <div class="card-body text-center">
-                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png" alt="card image"></img></p>
+                                    <p><img class=" img-fluid" src="https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1475836144/gvs96ywgysb53ivoaxs5.png" alt="card image"></img></p>
                                     <h4 class="card-title">Joyería</h4>
-                                    <p class="card-text">This is basic card with image on top, title, description and button.</p>
+                                    <p class="card-text">Productos exclusivos de diseños especiales directamente dentro del crucero</p>
                                     <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
                                 </div>
                             </div>
@@ -24,8 +24,8 @@ class Home extends React.Component{
                         <div class="backside">
                             <div class="card">
                                 <div class="card-body text-center mt-4">
-                                    <h4 class="card-title">Electrónicos</h4>
-                                    <p class="card-text">This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.</p>
+                                    <h4 class="card-title">Horarios e información</h4>
+                                    <p class="card-text">Abierto de lunes a jueves 9:00 a.m - 5:00 p.m</p>
                                     <ul class="list-inline">
                                         <li class="list-inline-item">
                                             <a class="social-icon text-xs-center" target="_blank" href="#">
@@ -61,7 +61,7 @@ class Home extends React.Component{
                         <div class="frontside">
                             <div class="card">
                                 <div class="card-body text-center">
-                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_02.png" alt="card image"></img></p>
+                                    <p><img class=" img-fluid" src="https://www.zilliondesigns.com/images/portfolio/gift-souvenir-shop/Logo-for-modern-online-gift-store-726668.png" alt="card image"></img></p>
                                     <h4 class="card-title">Souvenirs</h4>
                                     <p class="card-text">This is basic card with image on top, title, description and button.</p>
                                     <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
@@ -108,7 +108,7 @@ class Home extends React.Component{
                         <div class="frontside">
                             <div class="card">
                                 <div class="card-body text-center">
-                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png" alt="card image"></img></p>
+                                    <p><img class=" img-fluid" src="https://cdn.palbin.com/users/31359/images/dolce-gabbana-logo-1547642202.jpg" alt="card image"></img></p>
                                     <h4 class="card-title">Ropa</h4>
                                     <p class="card-text">This is basic card with image on top, title, description and button.</p>
                                     <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
@@ -155,7 +155,7 @@ class Home extends React.Component{
                         <div class="frontside">
                             <div class="card">
                                 <div class="card-body text-center">
-                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_04.jpg" alt="card image"></img></p>
+                                    <p><img class=" img-fluid" src="https://www.easyparapharmacie.es/media/easysoft/home/manufacturer/logo_lrp.jpg" alt="card image"></img></p>
                                     <h4 class="card-title">Cuidado de la piel</h4>
                                     <p class="card-text">This is basic card with image on top, title, description and button.</p>
                                     <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
@@ -202,7 +202,7 @@ class Home extends React.Component{
                         <div class="frontside">
                             <div class="card">
                                 <div class="card-body text-center">
-                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_05.png" alt="card image"></img></p>
+                                    <p><img class=" img-fluid" src="http://www.centenariocc.com/wp-content/uploads/2014/05/rivera-05-800x588.jpg" alt="card image"></img></p>
                                     <h4 class="card-title">Perfumería</h4>
                                     <p class="card-text">This is basic card with image on top, title, description and button.</p>
                                     <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
@@ -249,7 +249,7 @@ class Home extends React.Component{
                         <div class="frontside">
                             <div class="card">
                                 <div class="card-body text-center">
-                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_06.jpg" alt="card image"></img></p>
+                                    <p><img class=" img-fluid" src="http://atlantisplaza.com/wp-content/uploads/2015/07/ATLANTIS-logo-pep.jpg" alt="card image"></img></p>
                                     <h4 class="card-title">Juguetería</h4>
                                     <p class="card-text">This is basic card with image on top, title, description and button.</p>
                                     <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
