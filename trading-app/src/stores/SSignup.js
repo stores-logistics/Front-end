1f11c8fcd0847612c14361e54476a87ab295783f
Iván Herrera~¡ <biherrerap@unal.edu.co>
2019-06-16 03:51:04 -0500

    Added stores register with password encryption

--- trading-app/src/stores/SSignup.js
+++ trading-app/src/stores/SSignup.js
@@ -2,30 +2,67 @@ import React from 'react';
 import '../styles/Login.css';
 import API_URL from '../Server'
 
+
 class SSignup extends React.Component{
 
     constructor(props){
         super(props);       
         this.state = {
+          nombre: '',
           username: '',
-          password: ''
+          password: '',
+          storeid: '',
         }
-        this.ProceedLogin = this.ProceedLogin.bind(this);
-        this.HandleLogin = this.HandleLogin.bind(this);
+        this.ProceedSignup = this.ProceedSignup.bind(this);
+        this.HandleSignup = this.HandleSignup.bind(this);
     }    
 
-    ProceedLogin(event){
+    Encrypt(){
+    var bodyParser = require('body-parser');
+    var bcrypt = require('bcrypt');
+    var usersDB = require('usersDB');
+        
+    app.use(bodyParser.json()) 
+    app.use(bodyParser.urlencoded({ extended: true })) 
+    
+    var BCRYPT_SALT_ROUNDS = 12;
+    app.post('/register', function (req, res, next) {
+      var username = req.body.username;
+      var password = req.body.password;
+    
+      bcrypt.hash(password, BCRYPT_SALT_ROUNDS)
+        .then(function(hashedPassword) {
+            return usersDB.saveUser(username, hashedPassword);
+        })
+        .then(function() {
+            res.send();
+        })
+        .catch(function(error){
+            console.log("Error saving user: ");
+            console.log(error);
+            next();
+        });
+    });
+    }
+
+    ProceedSignup(event){
         switch(event.target.name){
+            case "nombre":
+                this.setState({nombre : event.target.value})
             case "username":
                     this.setState({username : event.target.value})
-            case "password":
+            case "contra":
                     this.setState({password : event.target.value})
+            case "storeid":
+                    this.setState({storeid : event.target.value})        
         }            
     }
-     
-    HandleLogin(){
-        console.log('Your input name value is: ' + this.state.username);
-        console.log('Your input value is: ' + this.state.password);
+      
+    HandleSignup(){
+        console.log('Your input name is: ' + this.state.nombre);
+        console.log('Your input user value is: ' + this.state.username);
+        console.log('Your input password value is: ' + this.state.password);
+        console.log('Your input storeid value is: ' + this.state.storeid);
 
         const axios = require("axios")
         axios.post(API_URL, {
@@ -57,24 +94,24 @@ class SSignup extends React.Component{
                                         <h4 className="card-title">Información de acceso</h4>
                                     <hr></hr>
                                     <div class="form-group-row">
-                                         <input name="inombre" id ="test_d" type="text" class="form-control" placeholder="Nombre" onChange={this.updateInput} ></input>
+                                         <input name="nombre" id ="test_d" type="text" class="form-control" placeholder="Nombre" onChange={this.ProceedSignup} ></input>
                                         <br></br>
-                                        <input name="indesc" id ="test_d" type="text" class="form-control" placeholder="username" onChange={this.updateInput} ></input>
+                                        <input name="username" id ="test_d" type="text" class="form-control" placeholder="username" onChange={this.ProceedSignup} ></input>
                                         <br></br>
                                       <div class="form-group row">
                                         <div class="col-6">
-                                            <input name="inombre" id ="test_d" type="password" class="form-control" placeholder="Contraseña" onChange={this.updateInput} ></input>
+                                            <input name="contra" id ="test_d" type="password" class="form-control" placeholder="Contraseña" onChange={this.ProceedSignup} ></input>
                                         </div> 
                                         <div class="col-6">
-                                             <input name="inombre" id ="test_d" type="password" class="form-control" placeholder="Repetir contraseña" onChange={this.updateInput} ></input>
+                                             <input id ="test_d" type="password" class="form-control" placeholder="Repetir contraseña" ></input>
                                          </div> 
                                      </div>
-                                     <input name="inombre" id ="test_d" type="text" class="form-control" placeholder="id de la tienda" onChange={this.updateInput} ></input>
+                                     <input name="storeid" id ="test_d" type="text" class="form-control" placeholder="id de la tienda" onChange={this.ProceedSignup} ></input>
                                      <div class ="row">
                                                 <div class="col-xs-6 col-md-9">
                                                 </div>
                                                 <div class="col-xs-6 col-md-3">
-                                                   <a onClick={this.HandleLogin}  id="verhcompras" onchange={this.ProceedLogin}class="btn"><i id ="nexte" class="fa fa-2x fa-arrow-circle-right" aria-hidden="true"></i></a>
+                                                   <a onClick={this.HandleSignup}  id="verhcompras" onchange={this.ProceedSignup}class="btn"><i id ="nexte" class="fa fa-2x fa-arrow-circle-right" aria-hidden="true"></i></a>
                                                 </div>
                                             </div> 
                                             </div>    
