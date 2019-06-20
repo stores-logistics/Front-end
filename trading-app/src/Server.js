1fa1f6a80c64a8caf3a26923ef9745df40b8efcf
Iván Herrera <biherrerap@unal.edu.co>
2019-06-19 21:43:31 -0500

    Change API url

--- trading-app/src/Server.js
+++ trading-app/src/Server.js
@@ -1,5 +1,4 @@
 //const API_URL="http://34.66.233.136/graphql";
-//const API_URL="http://35.193.85.93/graphql";
-
-const API_URL="http://34.66.233.136/graphql";
+const API_URL="http://35.193.85.93/graphql";
+//const API_URL="http://34.66.233.136/graphql";
 export default API_URL;
