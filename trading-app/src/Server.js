9af70368909651ca31389aeea10d12f91f6775f2
Ivan Herrera <biherrerap@unal.edu.co>
2019-07-09 10:29:29 -0500

    Fixed background image error

--- trading-app/src/Server.js
+++ trading-app/src/Server.js
@@ -4,9 +4,13 @@
 //const API_URL="http://35.193.85.93/graphql";
 
 //esta api tiene los roles
-const API_URL="http://34.66.233.136/graphql";
+//const API_URL="http://34.66.233.136/graphql";
 
 //esta api está libre
 //const API_URL="http://35.188.171.149:5000/graphql";
+
+//esta api es la nueva
+const API_URL="http://34.68.2.153:5001/graphql";
+
 
 export default API_URL;
