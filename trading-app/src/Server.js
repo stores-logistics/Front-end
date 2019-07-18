8984deb11de7c60f1cefcd1e9f11376a27dcd01f
Ivan Herrera <biherrerap@unal.edu.co>
2019-07-18 09:04:09 -0500

    Fixed error

--- trading-app/src/Server.js
+++ trading-app/src/Server.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-const API_URL="http://35.184.233.241:5001/graphql";
-=======
+ 
 //esta api es la nueva
 const API_URL="http://35.184.233.241:5001/graphql";
-
->>>>>>> a7bc42a87f6b5f934f650b0337caec7d9394875e
 
 export default API_URL;
